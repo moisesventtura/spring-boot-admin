e157a2ad7df187ef06bd175b23344f72eab41372
moisesventtura <moisesventura15@gmail.com>
2021-06-28 17:50:17 -0300

    Projeto Spring Boot Admin

---
+++ src/test/java/br/com/alura/springbootadmin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.alura.springbootadmin;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
